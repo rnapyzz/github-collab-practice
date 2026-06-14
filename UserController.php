8ec3d6d954c97b9492d2a60b93cddf0919ca400f
rnapyzz <96988008+rnapyzz@users.noreply.github.com>
2026-06-14 13:40:26 +0900

    Fix review comments: Add validation and hash password

--- UserController.php
+++ UserController.php
@@ -9,17 +9,23 @@ class UserController extends Controller
 {
     public function index()
     {
-        $users = User::all();
+        $users = User::latest()->paginate(20);
         return view('users.index', ['users' => $users]);
     }
 
     public function store(Request $request)
     {
-        $user = new User;
-        $user->name = $request->name;
-        $user->email = $request->email;
-        $user->password = $request->password;
-        $user->save();
+        $validate = $request->validate([
+            'name' => 'required|max:50',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:8',
+        ]);
+
+        User::create([
+            'name' => $validate['name'],
+            'email' => $validate['email'],
+            'password' => Hash::make($validate['password']),
+        ]);
 
         return redirect('/users');
     }
